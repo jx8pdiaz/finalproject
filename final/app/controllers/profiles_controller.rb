8ca42c5cbbd5bf02c940da4c2f201ce70188bcee
Rick Diaz <rdiaz020@fiu.edu>
2016-02-22 12:25:55 -0500

    Added new and create methods in Profiles controller

--- final/app/controllers/profiles_controller.rb
+++ final/app/controllers/profiles_controller.rb
@@ -3,6 +3,19 @@ class ProfilesController < ApplicationController
 		id = params[:id]
 	end
 
+	def new
+		@profile = @user.profiles.new
+
+	def create
+		@profile = @user.profiles.new
+		if @profile.save
+			redirect_to(user_profile_path(@user))
+		else
+			flash.now[:error] = @profile.errors.full_messages
+			render 'new'
+		end
+	end
+
 	private
 	def profile_params
 		@profile = Profile.find_by(id: params[:id])
