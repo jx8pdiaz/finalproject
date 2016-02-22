d04578475684e8240dcfe18798d378ed68ed43f0
Rick Diaz <rdiaz020@fiu.edu>
2016-02-22 15:58:51 -0500

    Generated Items controller and created Items routes

--- final/config/routes.rb
+++ final/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  resources :profiles
+  resources :profiles, :items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
