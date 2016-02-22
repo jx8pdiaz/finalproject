72f6ebf8ab1ec54218aab3a894aa4fd31a71f913
Rick Diaz <rdiaz020@fiu.edu>
2016-02-22 17:08:53 -0500

    Added before_action set_profile to Items controller

--- final/app/controllers/items_controller.rb
+++ final/app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
+	before_action :set_profile
+	
+	def new
+		@item = @profile.items.new
+	end
+
 	def create
 		@item = @profile.items.new(item_params)
 		if @item.save
@@ -6,9 +12,14 @@ class ItemsController < ApplicationController
 		else
 			flash.now[:error] = @item.errors.full_messages
 			render 'new'
+		end
 	end
 
 	private
+
+	def set_profile
+		@profile = Profile.find_by(id: params[:id])
+	end
 
 	def item_params
 		params.require(:item).permit(:name, :description, :pic_url, :download_url, :isForSale, :price)
