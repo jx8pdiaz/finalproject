a988a2ce4ba52bfa6934c4b02b1b2e948202d257
Rick Diaz <rdiaz020@fiu.edu>
2016-02-22 16:09:32 -0500

    Added create method to Items controller using item_params

--- final/app/controllers/items_controller.rb
+++ final/app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
+	def create
+		@item = @profile.items.new(item_params)
+		if @item.save
+			redirect_to profile_items_path(@profile)
+		else
+			flash.now[:error] = @item.errors.full_messages
+			render 'new'
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit(:name, :description, :pic_url, :download_url, :isForSale, :price)
+	end
 end
