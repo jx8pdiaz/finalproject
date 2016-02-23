336c09e1773e54a498289ba76f582ac5b1cddcd7
Rick Diaz <rdiaz020@fiu.edu>
2016-02-23 15:20:32 -0500

    Added column profile_id to Comments table

---
+++ final/db/migrate/20160223201950_add_profile_id_to_comments.rb
@@ -0,0 +1,5 @@
+class AddProfileIdToComments < ActiveRecord::Migration
+  def change
+    add_column :comments, :profile_id, :integer
+  end
+end
--- final/db/schema.rb
+++ final/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223190428) do
+ActiveRecord::Schema.define(version: 20160223201950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160223190428) do
     t.string   "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "profile_id"
   end
 
   create_table "item_types", force: :cascade do |t|
