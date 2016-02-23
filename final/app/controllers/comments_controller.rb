64f0b224f9b3c1d49aeb7cf5c15351d1ff4ac22d
Rick Diaz <rdiaz020@fiu.edu>
2016-02-23 15:19:14 -0500

    Added new and create methods to Comments controller using strong params

--- final/app/controllers/comments_controller.rb
+++ final/app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+	before_action :set_profile
+	def new
+		@comment = @profile.comments.new
+	end
+
+	def create
+		@profile=Profile.find_by(id: params[:profile_id])
+		@comment = @profile.comments.create(comment_params)
+		if @comment.save
+			redirect_to profile_comments_path(@profile)
+		else
+			flash.now[:error] = @comment.errors.full_messages
+			render 'new'
+		end
+	end
+
+	private
+
+	def set_profile
+		@profile = Profile.find_by(id: params[:id])
+	end
+
+	def comment_params
+		params.require(:comment).permit(:body, :subject)
+	end
+
 end
